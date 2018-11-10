d21caabaa823adda18d23c5cb83f4816bef872ad
Ashish Subedi <subediashish35@gmail.com>
2018-11-10 09:22:14 +0545

    settings.py file

---
+++ settings.py
@@ -0,0 +1,12 @@
+# Database configuration
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Your_Database',
+        'USER': 'username',
+        'PASSWORD': 'Password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
